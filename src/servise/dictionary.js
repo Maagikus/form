fb50b4108903eddaf9b86fd2b9f13f2614e48a5d
Ян Горинчой <yan@MacBook-Pro-An.local>
2023-07-02 23:13:54 +0300

    clear scss

--- src/servise/dictionary.js
+++ src/servise/dictionary.js
@@ -7,7 +7,7 @@ export const dictionary = {
       emptyCard: "Карта не может быть пустая",
       emptyExpDate: "Дата не может быть пустая",
       emptyCvv: "cvv не может быть пустым",
-      emptyCardHolder: "cvv не может быть пустым",
+      emptyCardHolder: "имя не может быть пустым",
       shortName: "Имя должно быть больше 2 символов",
       shortCvv: "Должно быть 3 символа",
       incorrectDate: "Дата введена некорректно",
