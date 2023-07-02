0bcff71e170c659495b9bb688029e3600ddd2546
Ян Горинчой <yan@MacBook-Pro-An.local>
2023-07-02 22:45:51 +0300

    clear cod and add dictionary

---
+++ src/servise/dictionary.js
@@ -0,0 +1,17 @@
+export const dictionary = {
+  ru: {
+    title: "Оплата заказа",
+    save: "Сохранить карту",
+    pay: "Оплатить",
+    errors: {
+      emptyCard: "Карта не может быть пустая",
+      emptyExpDate: "Дата не может быть пустая",
+      emptyCvv: "cvv не может быть пустым",
+      emptyCardHolder: "cvv не может быть пустым",
+      shortName: "Имя должно быть больше 2 символов",
+      shortCvv: "Должно быть 3 символа",
+      incorrectDate: "Дата введена некорректно",
+      incorrectCard: "Проверьте номер карты",
+    },
+  },
+};
